a2b428845042d71df360d60d5ee7bd9d47e344e6
dheerajprakria <dheeraj@prakria.com>
2024-09-23 12:14:30 +0530

    changes

--- src/components/Know-more/Hero.jsx
+++ src/components/Know-more/Hero.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 function Hero() {
   return (
-    <div className="container-fluid text-center know-more-hero m-auto d-flex align-items-center justify-content-center">
-      <div>
+    <div className="container-fluid  know-more-hero m-auto d-flex align-items-center">
+      <div className="container">
         <h1 className="text-white">Know More</h1>
         <p className="text-white">
           Our services are designed to address a wide range of <br></br>pain issues, from
